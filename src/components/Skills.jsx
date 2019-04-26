0d2efbd72c160a4651adeffa06f5ab925e466da6
Julian <julian@minka.io>
2019-04-25 23:46:30 -0500

    feat(Certificates/Education/Experience/Skills) Add style using styled-component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import H2Styled from '../styled/H2Styled';
+import H3Styled from '../styled/H3Styled';
 
-const Skills = () => {
-    return (
-        <h1>Skills component</h1>
-    );
+const Skills = props => {
+	return (
+		<div className='Skills'>
+			<H2Styled name='Skills' />
+			<div className='Skills-container'>
+				{
+					props.data && props.data.map((skill, index) => (
+						<div className='Skills-item' key={`Edu-${index}`} >
+							<H3Styled>
+								{skill.name}: {skill.percentage}
+							</H3Styled>
+						</div>
+					))
+				}
+			</div>
+		</div>
+	);
 }
 
 export default Skills;
